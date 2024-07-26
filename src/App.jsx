f6dcd427ec00e86deac5296f03761f0471cedccf
ChanUi <hcu55@naver.com>
2024-07-26 12:27:17 +0900

    Todo UI 세팅, CSS 레이아웃 작성, 절대경로(@) 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import TodoBody from './components/todos/TodoBody'
+import TodoHeader from './components/todos/TodoHeader'
+import DefaultLayout from './layouts/DefaultLayout'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Todos
-      </h1>  
+      <DefaultLayout>
+        <header>
+          <div className="flex justify-center">
+            <a to="/" className='flex'>
+              <h1 className='animate-bounce-slow py-8 text-red-200 max-w-max text-7xl'>todos</h1>
+            </a>
+          </div>
+        </header>
+        <section className="max-w-xl m-4 mx-auto">
+          <TodoHeader />
+          <TodoBody />
+        </section>
+      </DefaultLayout> 
     </>
   )
 }
