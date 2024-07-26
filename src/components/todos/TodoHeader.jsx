aa2672247b2d01adbf9a9e58eb2b2540ac6e626a
ChanUi <hcu55@naver.com>
2024-07-26 14:29:31 +0900

    Todo 등록 기능 구현

--- src/components/todos/TodoHeader.jsx
+++ src/components/todos/TodoHeader.jsx
@@ -1,15 +1,30 @@
+import { useState } from 'react';
 import TodoFilter from './TodoFilter'
+import { createPortal } from 'react-dom';
+import Modal from '../ui/Modal';
+import TodoForm from './TodoForm';
 
 // 함수형 컴포넌트(TodoHeader)
-const TodoHeader = () => {
-    return (
-        <div className="flex items-center justify-between mb-2" id="task-control">
-        <button className="animate-bounce-slow px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
-                data-cy="add-todo-button">Add Todo
-        </button>
-        <TodoFilter />
-      </div> 
-    )
+const TodoHeader = ({ onAdd }) => {
+  const [openModal, open] = useState(false);
+  const closeModal = () => open(false);
+  
+  return (
+    <div className="flex items-center justify-between mb-2" id="task-control">
+    <button 
+      className="animate-bounce-slow px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
+      data-cy="add-todo-button"
+      onClick={() => open(true)}
+      >Add Todo
+    </button>
+    {/* Modal 호출 부분 */}
+    {openModal && createPortal(
+      <Modal onClose={closeModal}>
+        <TodoForm onAdd={onAdd} onClose={closeModal}/>
+      </Modal>, document.body)}
+    <TodoFilter />
+    </div> 
+  )
 }
 
 export default TodoHeader;
