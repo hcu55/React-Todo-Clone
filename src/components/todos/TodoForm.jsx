3384eca1205c3271cc526eae2c7f72966c69481b
ChanUi <hcu55@naver.com>
2024-07-26 16:29:15 +0900

    Form 유효성 검사(useEffect 사용)

--- src/components/todos/TodoForm.jsx
+++ src/components/todos/TodoForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TODO_CATEGORY_ICON } from '@/constants/icon'
 
 const TodoForm = ({ onAdd, onUpdate, onClose, children, todo }) => {
@@ -9,9 +9,18 @@ const TodoForm = ({ onAdd, onUpdate, onClose, children, todo }) => {
 	const [title, setTitle] = useState(isNewTodoForm(children) ? '' : todo.title);
 	const [summary, setSummary] = useState(isNewTodoForm(children) ? '' : todo.summary);
 	const [category, setCategory] = useState(isNewTodoForm(children) ? 'TODO' : todo.category);
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	useEffect(() => {
+		if (title.trim() === '' || summary.trim() === '') {
+			setIsFormValid(false);
+		} else {
+			setIsFormValid(true);
+		}
+	}, [title, summary]);
 
 	const addOrUpdateTodoHandler = () => {
-		if (isNewTodoForm(children)) {
+		if (isFormValid && isNewTodoForm(children)) {
 			// Add 로직
 			const newTodo = {title, summary, category};
 			onAdd(newTodo);
@@ -52,10 +61,10 @@ const TodoForm = ({ onAdd, onUpdate, onClose, children, todo }) => {
 						<option value='DONE'>{TODO_CATEGORY_ICON.DONE} Done</option>
 					</select>
 				</div>
-				{/* {isFormInValid && <div className='mt-2 text-red-500'>모든 항목을 채워서 작성해주세요</div>} */}
+				{!isFormValid && <div className='mt-2 text-red-500'>모든 항목을 채워서 작성해주세요</div>}
 				<div className='flex justify-end gap-4'>
 					<button className='text-xl text-white' type='button' onClick={onClose}>Cancel</button>
-					<button className='px-6 py-3 text-xl text-red-200' type='button' onClick={addOrUpdateTodoHandler}> 
+					<button className='px-6 py-3 text-xl text-red-200' type='button' onClick={addOrUpdateTodoHandler} disabled={!isFormValid}> 
 						{isNewTodoForm(children) ? 'Add' : 'Update'}
           </button>  
 				</div>
